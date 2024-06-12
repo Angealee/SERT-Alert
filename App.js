ddb951624a2347fa4a388dc52cdbc1e58c41e858
Koby Macale <koby.macale24@gmail.com>
2024-06-12 21:07:27 +0800

    Did the (npm run reset-project) to remove the boilerplate and move the existing files in app to app-example

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to SERT Alert!</Text>
+      <Text>SERT Alert!</Text>
     </View>
   );
 }
